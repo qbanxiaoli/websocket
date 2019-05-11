a1da216a8a225f05cec2afde24d45e183dd2a216
mofangvr <330192168@qq.com>
2019-05-11 19:28:09 +0800

    fix 集成websocket

---
+++ src/main/java/com/controller/WebSocketController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mofangvr
+ * @description
+ * @create 2019-05-11 13:55
+ */
+@RestController
+@RequestMapping("/websocket")
+public class WebSocketController {
+
+
+
+}
